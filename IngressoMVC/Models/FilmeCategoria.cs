1fe4d5d1ff3a054110557f8fcd55d029c95dd927
EltonSenac <elsbueno6@gmail.com>
2022-05-24 19:10:43 -0300

    Configuracao do Banco de Dados

--- IngressoMVC/Models/FilmeCategoria.cs
+++ IngressoMVC/Models/FilmeCategoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,8 +13,10 @@ namespace IngressoMVC.Models
             FilmeId = filmeId;
             CategoriaId = categoriaId;
         }
+        [Key]
         public int FilmeId { get; set; }
         public Filme Filme { get; set; }
+        [Key]
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
     }
